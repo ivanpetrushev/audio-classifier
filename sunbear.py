73d9f8eb594c8f9cd177ede1effffeefdfc38e30
Ivan Petrushev <ivanatora@gmail.com>
2020-03-28 12:29:55 +0200

    Saving tag text

--- sunbear.py
+++ sunbear.py
@@ -30,7 +30,10 @@ class MusicPlayer(tk.Frame):
         self.btn_add_listbox = None
         self.btn_remove_listbox = None
         self.btn_tag_start_stop = None
-        self.text_tag_start_stop = None
+        self.btn_tag_start_stop_text = None
+        self.btn_save_tag = None
+        self.field_tags = None
+        self.field_tags_value = None
 
         # Call these methods
         self.get_audiofile_metadata()
@@ -70,16 +73,25 @@ class MusicPlayer(tk.Frame):
         self.btn_stop = tk.Button(self, text='Stop', command=self.stop)
         self.btn_stop.pack()
 
-        self.text_tag_start_stop = tk.StringVar()
-        self.text_tag_start_stop.set('Tag Start')
-        self.btn_tag_start_stop = tk.Button(self, textvariable=self.text_tag_start_stop, command=self.tag_start_stop)
-        self.btn_tag_start_stop.pack()
-
         self.slider_value = tk.DoubleVar()
         self.slider = tk.Scale(self, to=self.track_length, orient=tk.HORIZONTAL, length=700,
                                resolution=0.5, showvalue=True, tickinterval=30, digit=4,
                                variable=self.slider_value, command=self.update_slider)
         self.slider.pack()
+
+        self.btn_tag_start_stop_text = tk.StringVar()
+        self.btn_tag_start_stop_text.set('Tag Start')
+        self.btn_tag_start_stop = tk.Button(self, textvariable=self.btn_tag_start_stop_text,
+                                            command=self.tag_start_stop)
+        self.btn_tag_start_stop.pack()
+
+        self.field_tags_value = tk.StringVar()
+        self.field_tags_value.set('Tags (comma separated)')
+        self.field_tags = tk.Entry(self, textvariable=self.field_tags_value)
+        self.field_tags.pack(fill=tk.BOTH, expand=1)
+
+        self.btn_save_tag = tk.Button(self, text='Save', command=self.save_tag)
+        self.btn_save_tag.pack()
 
         self.btn_add_listbox = tk.Button(self, text='Add', command=self.add_data)
         self.btn_add_listbox.pack()
@@ -88,13 +100,17 @@ class MusicPlayer(tk.Frame):
 
         list_scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
         self.listbox = tk.Listbox(self, yscrollcommand=list_scrollbar.set)
-        self.listbox.bind('<Double-Button-1>', self.seek_item_start)
+        self.listbox.bind('<Double-Button-1>', self.select_item)
         list_scrollbar.config(command=self.listbox.yview)
         list_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
     def add_data(self):
-        self.data.append({'start': 12, 'end': 15, 'tag': 'Bau ' + str(len(self.data))})
+        self.data.append({
+            'start': 12,
+            'end': 15,
+            'tag': 'Bau ' + str(len(self.data))
+        })
         self.update_listbox()
 
     def remove_data(self):
@@ -135,14 +151,19 @@ class MusicPlayer(tk.Frame):
         else:
             print('Track Ended')
 
-    def seek_item_start(self, *event):
+    def select_item(self, *event):
         sel = self.listbox.curselection()
         if len(sel) == 0:
             return
         idx = sel[0]
         start = self.data[idx]['start']
-        print('start', start)
+        end = self.data[idx]['end']
+        tag = self.data[idx]['tag']
+        self.current_tag_start = start
+        self.current_tag_end = end
         self.slider_value.set(start)
+        self.field_tags_value.set(tag)
+        self.btn_tag_start_stop_text.set('Tag Stop')
 
     def update_slider(self, value):
         '''Move slider position when tk.Scale's trough is clicked or when slider is clicked.'''
@@ -167,14 +188,23 @@ class MusicPlayer(tk.Frame):
     def tag_start_stop(self):
         if not self.current_tag_start:
             self.current_tag_start = self.slider_value.get()
-            self.text_tag_start_stop.set('Tag Stop')
+            self.btn_tag_start_stop_text.set('Tag Stop')
             return
         self.current_tag_end = self.slider_value.get()
-        self.data.append({'start': self.current_tag_start, 'end': self.current_tag_end, 'tag': 'Bau ' + str(len(self.data))})
+        self.player.music.stop()
+
+    def save_tag(self):
+        self.data.append({
+            'start': self.current_tag_start,
+            'end': self.current_tag_end,
+            'tag': self.field_tags_value.get()
+        })
         self.current_tag_start = None
         self.current_tag_end = None
-        self.text_tag_start_stop.set('Tag Start')
+        self.field_tags_value.set('Tags (comma separated)')
+        self.btn_tag_start_stop_text.set('Tag Start')
         self.update_listbox()
+
 
 if __name__ == "__main__":
     root = tk.Tk()  # Initialize an instance of Tk window.
